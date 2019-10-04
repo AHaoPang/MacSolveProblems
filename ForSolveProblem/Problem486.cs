57741f63ab1655f01272983770fe53f84260565d
HaoMac <mr_coder@qq.com>
2019-10-04 23:21:49 +0800

    486 494

---
+++ ForSolveProblem/Problem486.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForSolveProblem
+{
+    public class Problem486 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = PredictTheWinner(new int[] { 1, 5, 2 });
+            if (temp != false) throw new Exception();
+
+            temp = PredictTheWinner(new int[] { 1, 5, 233, 7 });
+            if (temp != true) throw new Exception();
+
+            temp = PredictTheWinner(new int[] { 1, 2 });
+            if (temp != true) throw new Exception();
+        }
+
+        public bool PredictTheWinner(int[] nums)
+        {
+            /*
+             * 题目概述：玩一个游戏，预测下首发是否是赢家
+             * 
+             * 思路：
+             *  1. 这是一个挑选数字后汇总的游戏
+             *  2. 每轮玩家都会有两个选择，玩家当然会选择让自己赢的那个选择了
+             *
+             * 关键点：
+             *
+             * 时间复杂度： O(n)
+             * 空间复杂度： O(n)
+             */
+
+            return BackTrace(nums, 0, nums.Length - 1, new List<int>(), new List<int>(), 1);
+        }
+
+
+        private bool BackTrace(int[] nums, int leftIndex, int rightIndex, IList<int> oneCollection, IList<int> twoCollection, int times)
+        {
+            if (leftIndex > rightIndex)
+            {
+                var countTemp = 0;
+                for (int i = 0; i < oneCollection.Count || i < twoCollection.Count; i++)
+                {
+                    if (i < oneCollection.Count && i < twoCollection.Count) countTemp += oneCollection[i] - twoCollection[i];
+                    else if (i < oneCollection.Count) countTemp += oneCollection[i];
+                    else countTemp -= twoCollection[i];
+                }
+
+                return countTemp >= 0;
+            }
+
+            var isOne = (times & 1) == 1;
+
+            //选左
+            var leftValue = nums[leftIndex];
+            if (isOne) oneCollection.Add(leftValue);
+            else twoCollection.Add(leftValue);
+            var resultTemp = BackTrace(nums, leftIndex + 1, rightIndex, oneCollection, twoCollection, times + 1);
+
+            if (isOne)
+            {
+                oneCollection.RemoveAt(oneCollection.Count - 1);
+                if (resultTemp) return true;
+            }
+            else
+            {
+                twoCollection.RemoveAt(twoCollection.Count - 1);
+                if (!resultTemp) return false;
+            }
+
+            //选右
+            var rightValue = nums[rightIndex];
+            if (isOne) oneCollection.Add(rightValue);
+            else twoCollection.Add(rightValue);
+
+            var resultTemp2 = BackTrace(nums, leftIndex, rightIndex - 1, oneCollection, twoCollection, times + 1);
+
+            if (isOne) oneCollection.RemoveAt(oneCollection.Count - 1);
+            else twoCollection.RemoveAt(twoCollection.Count - 1);
+
+            return resultTemp2;
+        }
+    }
+}
---
+++ ForSolveProblem/Problem494.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForSolveProblem
+{
+    public class Problem494 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = FindTargetSumWays(new int[] { 1, 1, 1, 1, 1 }, 3);
+            if (temp != 5) throw new Exception();
+
+            temp = FindTargetSumWays(new int[] { 1 }, 2);
+            if (temp != 0) throw new Exception();
+
+            temp = FindTargetSumWays(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 1);
+            if (temp != 256) throw new Exception();
+        }
+
+        public int FindTargetSumWays(int[] nums, int S)
+        {
+            var sumTemp = nums.Sum();
+            if (sumTemp < S) return 0;
+
+            var dp = new int[nums.Length, 2 * sumTemp + 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j <= 2 * sumTemp; j++)
+                {
+                    if (i == 0)
+                    {
+                        dp[i, nums[i] + sumTemp] += 1;
+                        dp[i, -nums[i] + sumTemp] += 1;
+                        break;
+                    }
+
+                    var sumTotal = 0;
+                    if (j - nums[i] >= 0) sumTotal += dp[i - 1, j - nums[i]];
+                    if (j + nums[i] <= 2 * sumTemp) sumTotal += dp[i - 1, j + nums[i]];
+                    dp[i, j] += sumTotal;
+                }
+            }
+
+            return dp[nums.Length - 1, S + sumTemp];
+        }
+
+        public int FindTargetSumWays1(int[] nums, int S)
+        {
+            /*
+             * 题目概述：n 个元素构造目标和的过程
+             * 
+             * 思路：
+             *  1. 每个元素都有两个选择：添加+号，添加-号
+             *  2. 当元素添加完符号以后，下一个数字就会有新的目标了
+             *  3. 父层元素期望返回达成目标的次数
+             *
+             * 关键点：
+             *
+             * 时间复杂度：O(2^n)
+             * 空间复杂度：O(2^n)
+             */
+
+            return Recursive(nums, 0, S, new Dictionary<string, int>());
+        }
+
+        private int Recursive(int[] nums, int curIndex, int targetValue, IDictionary<string, int> dic)
+        {
+            var keyTemp = $"{curIndex}_{targetValue}";
+            if (dic.ContainsKey(keyTemp)) return dic[keyTemp];
+
+            if (curIndex == nums.Length) return targetValue == 0 ? 1 : 0;
+
+            var v1 = Recursive(nums, curIndex + 1, targetValue - nums[curIndex], dic);
+            var v2 = Recursive(nums, curIndex + 1, targetValue + nums[curIndex], dic);
+
+            dic[keyTemp] = v1 + v2;
+            return v1 + v2;
+        }
+    }
+}
