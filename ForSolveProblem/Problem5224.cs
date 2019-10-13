22fd634d5f5f79c9b813450496dc94639e07715a
HaoMac <mr_coder@qq.com>
2019-10-13 22:10:23 +0800

    5224

--- ForSolveProblem/Problem5224.cs
+++ ForSolveProblem/Problem5224.cs
@@ -1,9 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ForSolveProblem
 {
     public class Problem5224 : IProblem
     {
+        public class calcEntity
+        {
+            public calcEntity(long lastNum)
+            {
+                this.totalNum = lastNum;
+                queueMem = new Queue<long>();
+                queueMem.Enqueue(lastNum);
+            }
+
+            public long totalNum;
+
+            public long newNum;
+
+            public long subNum;
+
+            public Queue<long> queueMem;
+        }
+
         public int DieSimulator(int n, int[] rollMax)
+        {
+            var constNum = unchecked((int)(1E9 + 7));
+
+            var numQueueArray = new List<calcEntity>();
+            for (int i = 0; i < 6; i++)
+                numQueueArray.Add(new calcEntity(1));
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int a = 0; a < 6; a++)
+                {
+                    var sumTemp = 0L;
+                    for (int b = 0; b < 6; b++)
+                    {
+                        if (a == b) continue;
+
+                        sumTemp += numQueueArray[b].totalNum;
+                        sumTemp %= constNum;
+                    }
+
+                    numQueueArray[a].queueMem.Enqueue(sumTemp);
+                    numQueueArray[a].newNum = sumTemp;
+
+                    if (numQueueArray[a].queueMem.Count > rollMax[a])
+                    {
+                        var v = numQueueArray[a].queueMem.Dequeue();
+                        numQueueArray[a].subNum = v;
+                    }
+                }
+
+                for (int c = 0; c < 6; c++)
+                {
+                    numQueueArray[c].totalNum += numQueueArray[c].newNum;
+                    numQueueArray[c].totalNum -= numQueueArray[c].subNum;
+                    numQueueArray[c].subNum = 0;
+                }
+            }
+
+            var forReturn = 0L;
+            foreach (var arrayItem in numQueueArray)
+            {
+                var tempValue = 0L;
+                while (arrayItem.queueMem.Any())
+                {
+                    tempValue += arrayItem.queueMem.Dequeue();
+                    tempValue %= constNum;
+                }
+
+                forReturn += tempValue;
+                forReturn %= constNum;
+            }
+
+            return (int)forReturn;
+        }
+
+        public int DieSimulator1(int n, int[] rollMax)
         {
             var constNum = unchecked((int)(1E9 + 7));
             var countArray = new int[] { 1, 1, 1, 1, 1, 1 };
@@ -30,7 +107,7 @@ namespace ForSolveProblem
                         newArray[j] %= constNum;
                     }
                 }
-                 
+
                 totalArray = newArray;
             }
 
