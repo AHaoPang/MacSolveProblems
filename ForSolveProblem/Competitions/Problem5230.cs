e312d37bb260399306627990f2233f42a2de9fc7
pyh <pangyanhao@beisen.com>
2019-10-22 22:20:39 +0800

    5230

--- ForSolveProblem/Competitions/Problem5230.cs
+++ ForSolveProblem/Competitions/Problem5230.cs
@@ -16,6 +16,19 @@ namespace ForSolveProblem
 
         public bool CheckStraightLine(int[][] coordinates)
         {
+            /*
+            * 问题:判断给定的点,是否在一条直线上
+            * 思路:
+            *  1.判断依据是:相邻点构成的线段,斜率相同时,就是在一条实现上了
+            *  2.例如连续的3个点,构成线段1和线段2,线段1的斜率为 X1/Y1 线段2的斜率为 X2/Y2 
+            *  3.如果 X1/Y1 = X2/Y2, 即X1*Y2 = X2*Y1时,这3个点就是在1条直线上的
+            * 
+            * 关键点:
+            * 
+            * 时间复杂度:O(n)
+            * 空间复杂度:O(1)
+            */
+
             var firstPoint = coordinates[0];
             var secondPoint = coordinates[1];
 
