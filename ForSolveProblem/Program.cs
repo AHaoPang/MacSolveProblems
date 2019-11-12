2a82375c99fc7444187c54d4a4d757dd39b89ac0
pyh <pangyanhao@beisen.com>
2019-11-12 21:52:10 +0800

    1254

--- ForSolveProblem/Program.cs
+++ ForSolveProblem/Program.cs
@@ -6,7 +6,7 @@ namespace ForSolveProblem
     {
         static void Main(string[] args)
         {
-            var p = new Problem1253();
+            var p = new Problem1254();
             p.RunProblem();
         }
     }
