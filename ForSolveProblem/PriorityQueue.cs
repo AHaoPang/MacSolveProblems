3a86fe0fa731ad962887657668d9ee4d7d63e2c0
pyh <pangyanhao@beisen.com>
2019-11-19 09:20:33 +0800

    新增优先级队列类

---
+++ ForSolveProblem/PriorityQueue.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForSolveProblem
+{
+    /// <summary>
+    /// 优先级队列
+    /// </summary>
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        #region private fields
+        /// <summary>
+        /// 内部维护的数组
+        /// </summary>
+        private IList<T> m_innerList;
+
+        /// <summary>
+        /// 大的优先,还是小的优先
+        /// </summary>
+        private bool m_isBigFirst;
+        #endregion
+
+        public PriorityQueue(bool isBigFirst)
+        {
+            m_innerList = new List<T>() { default(T) };
+            m_isBigFirst = isBigFirst;
+        }
+
+        public void AddData(T t)
+        {
+            m_innerList.Add(t);
+            var curIndex = m_innerList.Count - 1;
+
+            DownToUp(m_innerList, curIndex);
+        }
+
+        public bool HasData() => m_innerList.Count >= 2;
+
+        public T GetData()
+        {
+            if (m_innerList.Count < 2) return default(T);
+
+            var forReturn = m_innerList[1];
+
+            m_innerList[1] = m_innerList[m_innerList.Count - 1];
+            m_innerList.RemoveAt(m_innerList.Count - 1);
+            UpToDown(m_innerList, 1);
+
+            return forReturn;
+        }
+
+        #region private functions
+        /// <summary>
+        /// 自上至下的更新
+        /// </summary>
+        private void UpToDown(IList<T> innerList, int curIndex)
+        {
+            if (curIndex >= innerList.Count) return;
+
+            var cur = innerList[curIndex];
+
+            var sonLeft = default(T);
+            var sonLeftIndex = curIndex * 2;
+            if (sonLeftIndex <= innerList.Count - 1) sonLeft = innerList[sonLeftIndex];
+
+            var sonRight = default(T);
+            var sonRightIndex = curIndex * 2 + 1;
+            if (sonRightIndex <= innerList.Count - 1) sonRight = innerList[sonRightIndex];
+
+            var compareResult = CompareAndReturnPriority(sonLeft, sonRight);
+            if (IsMove(cur, compareResult))
+            {
+                var moveIndex = -1;
+                if (ReferenceEquals(compareResult, sonLeft))
+                    moveIndex = sonLeftIndex;
+                else if (ReferenceEquals(compareResult, sonRight))
+                    moveIndex = sonRightIndex;
+
+                if (moveIndex != -1)
+                {
+                    Swap(innerList, moveIndex, curIndex);
+                    UpToDown(innerList, moveIndex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 自下至上的更新
+        /// </summary>
+        private void DownToUp(IList<T> innerlist, int curIndex)
+        {
+            var fatherIndex = curIndex / 2;
+            if (fatherIndex < 1) return;
+
+            if (IsMove(innerlist[curIndex], innerlist[fatherIndex]))
+            {
+                Swap(innerlist, curIndex, fatherIndex);
+                DownToUp(innerlist, fatherIndex);
+            }
+        }
+
+        /// <summary>
+        /// 交换列表中两个索引的位置
+        /// </summary>
+        private void Swap(IList<T> list, int oneIndex, int secondIndex)
+        {
+            var temp = list[oneIndex];
+            list[oneIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+
+        /// <summary>
+        /// 判断是否有交换源和目标的位置
+        /// </summary>
+        private bool IsMove(T sourcePos, T targetPos)
+        {
+            var compareResult = sourcePos.CompareTo(targetPos);
+
+            if (m_isBigFirst && compareResult > 0) return true;
+            if (!m_isBigFirst && compareResult < 0) return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 比较并返回优先级大的那个对象
+        /// </summary>
+        private T CompareAndReturnPriority(T oneT, T secondT)
+        {
+            var compareResult = oneT.CompareTo(secondT);
+
+            if (m_isBigFirst) return compareResult > 0 ? oneT : secondT;
+
+            return compareResult < 0 ? oneT : secondT;
+        }
+        #endregion
+    }
+}
---
+++ ForSolveProblem/Problem1263.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForSolveProblem
+{
+    public class Problem1263 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
