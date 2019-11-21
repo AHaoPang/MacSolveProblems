b8fd657a206d25b2896a81635a9213809b4d7197
pyh <pangyanhao@beisen.com>
2019-11-21 12:58:52 +0800

    简单精简

--- ForSolveProblem/Problem1263.cs
+++ ForSolveProblem/Problem1263.cs
@@ -49,10 +49,36 @@ namespace ForSolveProblem
                 new char[]{'#', '#', '#', '#', '#', '#', '#' }
             });
             if (temp != -1) throw new Exception();
+
+            temp = MinPushBox(new char[][]
+            {
+                new char[]{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#'},
+                new char[]{'#', '.', '.', '.', '#', '#', '.', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#', '#', 'T', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '#', '#', '#', '.', '#', '#', '.', '#'},
+                new char[]{'#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '#', '#', '.', '#'},
+                new char[]{'#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '#', '#', '.', '#'},
+                new char[]{'#', '.', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '.', '#', '#', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', 'B', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '.', '.', '.', '.', '.', '.', '.', 'S', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
+                new char[]{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+            });
+            if (temp != 29) throw new Exception();
         }
 
         public int MinPushBox(char[][] grid)
         {
+            //1.确定人的位置 和 箱子的位置 并把他们的位置修改为地面
             var boxPos = new int[2];
             var personPos = new int[2];
             for (int r = 0; r < grid.Length; r++)
@@ -63,8 +89,10 @@ namespace ForSolveProblem
                     {
                         boxPos = new int[] { r, c };
                         grid[r][c] = '.';
+                        continue;
                     }
-                    else if (grid[r][c] == 'S')
+                    
+                    if (grid[r][c] == 'S')
                     {
                         personPos = new int[] { r, c };
                         grid[r][c] = '.';
@@ -72,6 +100,7 @@ namespace ForSolveProblem
                 }
             }
 
+            //2.设定BFS的初始状态
             var initStatus = new CurStatus()
             {
                 CurSteps = 0,
@@ -80,12 +109,13 @@ namespace ForSolveProblem
                 PersonRow = personPos[0],
                 PersonCol = personPos[1]
             };
-            var pq = new PriorityQueue<CurStatus>(true);
+            var pq = new PriorityQueue<CurStatus>(false);
             pq.AddData(initStatus);
 
+            //3.开始做BFS,并使用优先队列做辅助
             var rows = grid.Length;
             var cols = grid[0].Length;
-            var statusHash = new HashSet<CurStatus>() { initStatus };
+            var statusArray = new int[rows, cols, rows, cols];
             var upDownArray = new int[] { -1, 1, 0, 0 };
             var leftRightArray = new int[] { 0, 0, -1, 1 };
             while (pq.HasData())
@@ -95,49 +125,40 @@ namespace ForSolveProblem
                 {
                     var personRowTemp = statusTemp.PersonRow + upDownArray[i];
                     var personColTemp = statusTemp.PersonCol + leftRightArray[i];
+                    var boxRowTemp = statusTemp.BoxRow;
+                    var boxColTemp = statusTemp.BoxCol;
+                    var stepsTemp = statusTemp.CurSteps;
 
+                    //1.校验箱子的移动符合逻辑
                     if (personRowTemp >= rows || personRowTemp < 0 || personColTemp >= cols || personColTemp < 0) continue;
                     if (grid[personRowTemp][personColTemp] == '#') continue;
 
+                    //2.人要推动箱子了
                     if (personRowTemp == statusTemp.BoxRow && personColTemp == statusTemp.BoxCol)
                     {
-                        var boxRowTemp = statusTemp.BoxRow + upDownArray[i];
-                        var boxColTemp = statusTemp.BoxCol + leftRightArray[i];
+                        boxRowTemp = statusTemp.BoxRow + upDownArray[i];
+                        boxColTemp = statusTemp.BoxCol + leftRightArray[i];
 
                         if (boxRowTemp >= rows || boxRowTemp < 0 || boxColTemp >= cols || boxColTemp < 0) continue;
                         if (grid[boxRowTemp][boxColTemp] == '#') continue;
-
                         if (grid[boxRowTemp][boxColTemp] == 'T') return statusTemp.CurSteps + 1;
 
-                        var newStatus = new CurStatus()
-                        {
-                            CurSteps = statusTemp.CurSteps + 1,
-                            BoxCol = boxColTemp,
-                            BoxRow = boxRowTemp,
-                            PersonCol = personColTemp,
-                            PersonRow = personRowTemp
-                        };
-                        if (!statusHash.Contains(newStatus))
-                        {
-                            pq.AddData(newStatus);
-                            statusHash.Add(newStatus);
-                        }
+                        stepsTemp++;
                     }
-                    else
+
+                    //3.标识当前这种新的状态
+                    var newStatus = new CurStatus()
                     {
-                        var newStatus = new CurStatus()
-                        {
-                            CurSteps = statusTemp.CurSteps,
-                            BoxCol = statusTemp.BoxCol,
-                            BoxRow = statusTemp.BoxRow,
-                            PersonCol = personColTemp,
-                            PersonRow = personRowTemp
-                        };
-                        if (!statusHash.Contains(newStatus))
-                        {
-                            pq.AddData(newStatus);
-                            statusHash.Add(newStatus);
-                        }
+                        CurSteps = stepsTemp,
+                        BoxCol = boxColTemp,
+                        BoxRow = boxRowTemp,
+                        PersonCol = personColTemp,
+                        PersonRow = personRowTemp
+                    };
+                    if (statusArray[newStatus.BoxRow, newStatus.BoxCol, newStatus.PersonRow, newStatus.PersonCol] != 1)
+                    {
+                        pq.AddData(newStatus);
+                        statusArray[newStatus.BoxRow, newStatus.BoxCol, newStatus.PersonRow, newStatus.PersonCol] = 1;
                     }
                 }
             }
@@ -166,27 +187,6 @@ namespace ForSolveProblem
                 if (this.CurSteps > other.CurSteps) return 1;
 
                 return -1;
-            }
-
-            public override int GetHashCode()
-            {
-                return BoxRow + BoxCol + PersonRow + PersonCol;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj is CurStatus)
-                {
-                    var anotherStatus = obj as CurStatus;
-
-                    return 
-                        anotherStatus.BoxRow == this.BoxRow &&
-                        anotherStatus.BoxCol == this.BoxCol &&
-                        anotherStatus.PersonRow == this.PersonRow &&
-                        anotherStatus.PersonCol == this.PersonCol;
-                }
-                else
-                    return base.Equals(obj);
             }
         }
     }
