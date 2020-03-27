63f5f6bf59b96d149bebacce2a378def1e10884f
HaoMac <mr_coder@qq.com>
2020-03-27 21:20:36 +0800

    tijiao

---
+++ ForSolveProblem/Problem832.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ForSolveProblem
+{
+    public class Problem832 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            return A.Select(i => i.Reverse().Select(j => j ^ 1).ToArray()).ToArray();
+        }
+
+        public int[][] FlipAndInvertImage1(int[][] A)
+        {
+            var res = new int[A.Length][];
+            for(var r = 0;r < A.Length; r++)
+                res[r] = A[r].Reverse().Select(i => i ^ 1).ToArray();
+
+            return res;
+        }
+    }
+}
---
+++ ForSolveProblem/Problem914.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace ForSolveProblem
+{
+    public class Problem914 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = HasGroupsSizeX(new[] { 0, 0, 1, 1, 1, 1, 2, 2, 3, 4 });
+
+        }
+
+        public bool HasGroupsSizeX(int[] deck)
+        {
+            var list = deck
+                .GroupBy(i => i)
+                .Select(i => i.Count());
+
+            var gcd = 0;
+            foreach (var listItem in list)
+            {
+                gcd = Gcd(gcd, listItem);
+                if (gcd == 1)
+                    return false;
+            }
+
+            return gcd != 1;
+        }
+
+        public bool HasGroupsSizeX1(int[] deck)
+        {
+            var orderDeck = deck.OrderBy(i => i).ToArray();
+
+            var curValue = orderDeck[0];
+            var curCount = 1;
+            var gcd = 0;
+            for (var i = 1; i < orderDeck.Length; i++)
+            {
+                var deckItem = orderDeck[i];
+                if (curValue != deckItem)
+                {
+                    gcd = Gcd(gcd, curCount);
+                    if (gcd == 1) return false;
+
+                    curValue = deckItem;
+                    curCount = 1;
+                    continue;
+                }
+
+                curCount++;
+            }
+
+            gcd = Gcd(gcd, curCount);
+            return gcd != 1;
+        }
+
+        private int Gcd(int a, int b) => b == 0 ? a : Gcd(b, a % b);
+    }
+}
