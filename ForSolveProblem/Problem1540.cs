b7b887e2def3ab428596dc75eadb2e0ca4dcec73
HaoMac <mr_coder@qq.com>
2020-08-22 21:40:00 +0800

    change

---
+++ ForSolveProblem/Problem1540.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ForSolveProblem
+{
+    public class Problem1540 : IProblem
+    {
+        public void RunProblem()
+        {
+            var temp = CanConvertString("input", "ouput", 9);
+        }
+
+        public bool CanConvertString(string s, string t, int k)
+        {
+            if (s.Length != t.Length) return false;
+
+            var arr = new int[26];
+            for (var i = 0; i < s.Length; i++)
+            {
+                var sub = (t[i] - s[i] + 26) % 26;
+                arr[sub]++;
+            }
+
+            var c = k / 26;
+            var m = k % 26;
+
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (i <= m)
+                {
+                    if (arr[i] > c + 1)
+                        return false;
+                }
+                else
+                {
+                    if (arr[i] > c)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
