25c1f4d799b03c74f931eef4c440eaf377ffd7e7
pyh <953879889@qq.com>
2020-08-30 14:43:16 +0800

    just try

---
+++ ForSolveProblem/Problem1529.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForSolveProblem
+{
+    public class Problem1529 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MinFlips(string target)
+        {
+            var res = 0;
+
+            var curChar = ' ';
+            for (var i = 0; i < target.Length; i++)
+            {
+                if (curChar != target[i])
+                {
+                    res++;
+                    curChar = target[i];
+                }
+            }
+
+            if (target[0] == '0')
+                res--;
+
+            return res;
+        }
+    }
+}
---
+++ ForSolveProblem/Problem1530.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForSolveProblem
+{
+    public class Problem1530 : IProblem
+    {
+        public void RunProblem()
+        {
+            var root = new TreeNode(1);
+            var two = new TreeNode(2);
+            var three = new TreeNode(3);
+            root.left = two;
+            root.right = three;
+            var four = new TreeNode(4);
+            two.right = four;
+
+            var temp = CountPairs(root, 3);
+        }
+
+        public int CountPairs(TreeNode root, int distance)
+        {
+            m_count = 0;
+            LeftRightRoot(root, distance);
+            return m_count;
+        }
+
+        private int m_count;
+
+        private List<int> LeftRightRoot(TreeNode root, int distance)
+        {
+            if (root == null) return new List<int>();
+            if (root.right == null && root.left == null)
+                return new List<int>() { 1 };
+
+            var leftLists = LeftRightRoot(root.left, distance);
+            var rightLists = LeftRightRoot(root.right, distance);
+
+            var res = new List<int>();
+            foreach (var leftItem in leftLists)
+                if (leftItem + 1 < distance)
+                    res.Add(leftItem + 1);
+
+            foreach (var rightItem in rightLists)
+                if (rightItem + 1 < distance)
+                    res.Add(rightItem + 1);
+
+            if (leftLists.Any() && rightLists.Any())
+                foreach (var leftItem in leftLists)
+                    foreach (var rightItem in rightLists)
+                        if (leftItem + rightItem <= distance)
+                            m_count++;
+
+            return res;
+        }
+    }
+}
