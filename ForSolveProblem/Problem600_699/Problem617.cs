f9cdf63e26c41b4111b80036a648692e34626a18
pyh <953879889@qq.com>
2020-09-23 20:05:57 +0800

    617

--- ForSolveProblem/Problem600_699/Problem617.cs
+++ ForSolveProblem/Problem600_699/Problem617.cs
@@ -8,7 +8,7 @@ namespace ForSolveProblem
             throw new NotImplementedException();
         }
 
-        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        public TreeNode MergeTrees1(TreeNode t1, TreeNode t2)
         {
             if (t1 == null || t2 == null) return t1 ?? t2;
 
@@ -17,6 +17,20 @@ namespace ForSolveProblem
             t1.right = MergeTrees(t1.right, t2.right);
 
             return t1;
+        }
+
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null) return null;
+
+            var root = new TreeNode(0);
+
+            root.val += t1?.val ?? 0;
+            root.val += t2?.val ?? 0;
+            root.left = MergeTrees(t1?.left, t2?.left);
+            root.right = MergeTrees(t1?.right, t2?.right);
+
+            return root;
         }
     }
 }
