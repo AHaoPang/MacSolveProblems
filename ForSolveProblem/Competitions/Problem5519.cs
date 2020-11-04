75295e4346f41bd9c703199e6c829e5eebb06b52
HaoMac <mr_coder@qq.com>
2020-11-05 07:59:53 +0800

    ready for learn

---
+++ ForSolveProblem/Competitions/Problem5519.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ForSolveProblem
+{
+    public class Problem5519 : IProblem
+    {
+        public void RunProblem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReorderSpaces(string text)
+        {
+            var words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var blankCount = text.Count(i => i == ' ');
+
+            if (words.Length != 1)
+            {
+                var perCount = blankCount / (words.Length - 1);
+                var restCount = blankCount % (words.Length - 1);
+
+                var str = new string(Enumerable.Repeat(' ', perCount).ToArray());
+                var restStr = new string(Enumerable.Repeat(' ', restCount).ToArray());
+
+                return string.Join(str, words) + restStr;
+            }
+            else
+            {
+                var restStr = new string(Enumerable.Repeat(' ', blankCount).ToArray());
+
+                return words[0] + restStr;
+            }
+        }
+    }
+}
